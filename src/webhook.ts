7feccde65311b613878695f71d8fb24476b45703
DrMint <29893320+DrMint@users.noreply.github.com>
2024-07-28 22:32:58 +0200

    Handle errors while revalidating

--- src/webhook.ts
+++ src/webhook.ts
@@ -7,12 +7,18 @@ import type { EndpointChange } from "src/shared/payload/webhooks";
 export const webhookHandler = async (changes: EndpointChange[]) => {
   const index = meili.index(MeiliIndexes.DOCUMENT);
 
+  console.log("[Webhook] Received the following message", changes);
+
   const documents: MeiliDocument[] = [];
   for (const change of changes) {
     await index.deleteDocuments({
       filter: `endpointCalled = "${change.url}"`,
     });
-    documents.push(...(await convertEndpointChangeToMeiliDocuments(change)));
+    try {
+      documents.push(...(await convertEndpointChangeToMeiliDocuments(change)));
+    } catch (e) {
+      console.log("[Webhook] Failure to revalidate", change.url);
+    }
   }
 
   console.log("[Webhook] Adding", documents.length, "documents to Meilisearch");
